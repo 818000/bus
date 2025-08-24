297bae509fc41bde7363e2615dabcbe8fde1e8f4
Kimi Liu <839536@gmail.com>
2025-08-24 14:10:10 +0800

    :construction_worker: Update project version and travis automation for notice and above

--- bus-validate/src/main/java/org/miaixz/bus/validate/metric/SizeMatcher.java
+++ bus-validate/src/main/java/org/miaixz/bus/validate/metric/SizeMatcher.java
@@ -33,7 +33,6 @@ import java.util.Map;
 import org.miaixz.bus.core.xyz.ObjectKit;
 import org.miaixz.bus.validate.Context;
 import org.miaixz.bus.validate.magic.Matcher;
-import org.miaixz.bus.validate.magic.annotation.Length;
 import org.miaixz.bus.validate.magic.annotation.Size;
 
 /**
