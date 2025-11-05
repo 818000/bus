2855047b59f771455703e6d7bc4dbbea49118db6
Kimi Liu <839536@gmail.com>
2025-11-05 10:00:28 +0800

    :passport_control: Update Pay Provider and Voucher authorization handling for Voucher and magic

--- bus-pay/src/main/java/org/miaixz/bus/pay/metric/wechat/entity/v2/CloseOrder.java
+++ bus-pay/src/main/java/org/miaixz/bus/pay/metric/wechat/entity/v2/CloseOrder.java
@@ -32,7 +32,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.miaixz.bus.pay.magic.Material;
+import org.miaixz.bus.pay.magic.Voucher;
 
 /**
  * Model for the Close Order API.
@@ -45,7 +45,7 @@ import org.miaixz.bus.pay.magic.Material;
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CloseOrder extends Material {
+public class CloseOrder extends Voucher {
 
     /**
      * Sub-merchant App ID (for service providers).
